0a6802326cadc391765e9d216fc7f4739d4f7527
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-09-06 11:31:03 -0300

    feat: adiciona model de usuario

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const getDb = require('../util/database').getDb
+
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    async save() {
+        const db = getDb()
+        return db.collection('users').insertOne(this)
+    }
+}
+
+module.exports = User
